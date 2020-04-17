c53cdb4a4cae3e1a61058914099a4df5c0ecb948
Michell Excellent <theofficesmichell@gmail.com>
2020-04-16 23:27:59 -0500

    Updates

---
+++ precache-manifest.3678b6600476e27b8382286154722662.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee62239e6e04ed12eb9a51c101d35547",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0578623b6febbb9b798d",
+    "url": "/static/css/2.a43b59a1.chunk.css"
+  },
+  {
+    "revision": "0578623b6febbb9b798d",
+    "url": "/static/js/2.34ab2313.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.34ab2313.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c15179cc47ad67303310",
+    "url": "/static/js/main.902782c4.chunk.js"
+  },
+  {
+    "revision": "c9f6631fa42e4cc6a496",
+    "url": "/static/js/runtime-main.ff5e80ac.js"
+  }
+]);
